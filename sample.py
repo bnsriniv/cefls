fd97879650d6150695bb9f694785d7e2da200cd9
Srinivas BN <srinivas.bn@intel.com>
2024-01-16 15:30:13 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,21 +1,21 @@
-# from flask import Flask
-# app = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
-# @app.route('/')
-# def hello_world():
-#   return 'Hey its Python Flask application!'
+@app.route('/')
+def hello_world():
+  return 'Hey its Python Flask application!'
 
-# if __name__ == '__main__':
-#   app.run()
+if __name__ == '__main__':
+  app.run()
 
 
-import logging
-from flask import Flask, current_app, render_template, redirect, url_for, request
-from flask_session import Session
-from pathlib import Path
-import app_config
-from ms_identity_web import IdentityWebPython
-from ms_identity_web.adapters import FlaskContextAdapter
-from ms_identity_web.errors import NotAuthenticatedError
-from ms_identity_web.configuration import AADConfig
+# import logging
+# from flask import Flask, current_app, render_template, redirect, url_for, request
+# from flask_session import Session
+# from pathlib import Path
+# import app_config
+# from ms_identity_web import IdentityWebPython
+# from ms_identity_web.adapters import FlaskContextAdapter
+# from ms_identity_web.errors import NotAuthenticatedError
+# from ms_identity_web.configuration import AADConfig
 
